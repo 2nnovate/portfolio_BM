33a71e8e3c20da02f067105c2864be55d295d2c8
2nnovate <40700595+2nnovate@users.noreply.github.com>
2018-07-27 06:12:38 +0900

    Add files via upload

---
+++ BM_pf/src/client/components/images/index.js
@@ -0,0 +1,30 @@
+import defaultBlank from './defaultBlank.png';
+import savedBlank from './savedBlank.png';
+import halfStar from './halfStar.png';
+import oneStar from './1star.png';
+import oneHalfStar from './1halfStar.png';
+import twoStar from './2star.png';
+import twoHalfStar from './2halfStar.png';
+import threeStar from './3star.png';
+import threeHalfStar from './3halfStar.png';
+import fourStar from './4star.png';
+import fourHalfStar from './4halfStar.png';
+import fiveStar from './5star.png';
+import heart from './heart.png';
+import share from './share.png';
+import filledStar from './filledStar.png';
+import blankedStar from './blankedStar.png';
+import fullheart from './fullheart.png';
+
+export {savedBlank, defaultBlank, halfStar, oneStar, twoStar
+, twoHalfStar
+, threeStar
+, threeHalfStar
+, fourStar
+, fourHalfStar
+, fiveStar
+, heart
+, share
+, filledStar
+, blankedStar
+, fullheart};
